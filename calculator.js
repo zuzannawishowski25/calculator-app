36d6070acd671704d1cb2317a9c5045f4cd0c52d
zuzannawishowski25 <112281335+zuzannawishowski25@users.noreply.github.com>
2025-03-19 20:40:29 -0400

    updates

---
+++ calculator.js
@@ -0,0 +1,20 @@
+// const numberButtons = document.querySelectorAll(".number_button");
+// const data = {
+//   runningSum: "",
+// };
+
+// function updateView(elementId) {
+//   document.getElementById(elementId).textContent = data.runningSum;
+//   console.log("updating view", data.runningSum);
+// }
+
+// function updateRunningSum(value) {
+//   data.runningSum += value;
+//   updateView("runningSum");
+// }
+
+// numberButtons.forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     updateRunningSum(e.target.textContent);
+//   });
+// });
